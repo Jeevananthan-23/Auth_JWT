3fafbd4b07aab3d88469714550aded7a9f4c3f01
Jeevananthan-23 <Jeeva023@outlook.com>
2022-06-28 20:03:36 +0530

    Athu_JWT-Template

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth_JWT.Repository
+{
+    public interface IUserRepository
+    {
+
+    }
+}
